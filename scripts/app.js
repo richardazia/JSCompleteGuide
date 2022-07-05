86f3a594634f2c5d81b1d8beb26d72a9c6ca009c
Richard Azia <richard@main-vision.com>
2022-07-05 11:55:36 +0200

    add vendor.js and styles and make the add function work

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,23 @@
-'use strict'
+console.log('/scripts/app.js')
+
 const defaultResult = 0;
 let currentResult = defaultResult;
+
+function getUserNumberInput() {
+    return parseInt(usrInput.value);
+}
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+addBtn.addEventListener('click', add);
+console.log(add);
+
+/*
 let addBtn;
 
 function add(num1, num2) {
@@ -23,3 +40,4 @@ currentResult = (currentResult + 10) * 3 / 2 - 1;
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
 
 outputResult(currentResult, calculationDescription);
+*/
