55fd555abf73146887e0ded75b9924cdb326177e
Richard Azia <richard@main-vision.com>
2022-07-07 12:52:37 +0200

    add attack handler

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10; // use caps to show that this is a constant global value
+const MONSTER_ATTACK_VALUE = 15;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -9,6 +10,15 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
     const damage = dealMonsterDamage(ATTACK_VALUE);
     currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        console.log('You won!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        console.log('You lost!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        console.log("You're both dead, no one wins. ;-)")
+    }
 }
 
 attackBtn.addEventListener('click', attackHandler);
