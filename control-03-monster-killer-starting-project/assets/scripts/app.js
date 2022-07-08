41d1c2cdc3c21f7e844a122798e54594f46c803b
Richard Azia <richard@main-vision.com>
2022-07-08 18:45:05 +0200

    use switch vice if and else if

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -33,6 +33,49 @@ function writeToLog(event, value, monsterHealth, playerHealth) {
         finalMonsterHealth: monsterHealth,
         finalPlayerHealth: playerHealth,
     };
+    switch(event) {
+        case LOG_EVENT_PLAYER_ATTACK:
+            logEntry.target = 'MONSTER';
+            break;
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry = {
+                event: event,
+                value: value,
+                target: 'MONSTER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth,
+            };
+            break;
+        case LOG_EVENT_MONSTER_ATTACK:
+            logEntry = {
+                event: event,
+                value: value,
+                target: 'PLAYER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth,
+            };
+            break;
+        case LOG_EVENT_PLAYER_HEAL:
+            logEntry = {
+                event: event,
+                value: value,
+                target: 'PLAYER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth,
+            };
+            break;
+        case LOG_EVENT_GAME_OVER:
+            logEntry = {
+                event: event,
+                value: value,
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth,
+            };
+            break;
+        default:
+            logEntry = {};
+    }
+    /*
     if (event === LOG_EVENT_PLAYER_ATTACK) {
         logEntry.target = 'MONSTER';
     } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
@@ -67,6 +110,7 @@ function writeToLog(event, value, monsterHealth, playerHealth) {
             finalPlayerHealth: playerHealth,
         };
     }
+    */
     battleLog.push(logEntry);
 }
 
